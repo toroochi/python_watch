a6e84176802550b4e682f600609c676ccaa0b584
toroochi <kanon.0982@gmail.com>
2023-10-03 15:26:08 +0900

    add : command入力欄

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 import PySimpleGUI as sg
 import datetime
 
+sg.theme('DarkPurple1')
 def getTime():
      return datetime.datetime.now().strftime('%-m/%-d %H:%M:%S')
 
-layout = [[sg.Text('', size=(19, 0), key='_time_',font=(30))]]
+layout = [[sg.Text('', size=(20, 2), key='_time_',font=(40))],[sg.Text('Command'),sg.InputText(size=(20,1))]]
 window = sg.Window('Clock', layout, grab_anywhere=True)
 
 while True:
